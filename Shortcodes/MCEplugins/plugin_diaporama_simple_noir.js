8b4f3e2a2b463a4541eb2d49c895e93f4be78ecf
Raphael <raphael@couleur-citron.com>
2015-03-05 12:11:31 +0100

    nouveau plugin avec style différent pour tinyMCE

---
+++ Shortcodes/MCEplugins/plugin_diaporama_simple_noir.js
@@ -0,0 +1,25 @@
+(function() {
+	tinymce.create('tinymce.plugins.Diaposimplenoir', {
+		init : function(ed, url) {
+			ed.addButton('Diaposimplenoir', {
+				title : 'Diaporama avec une seule image et fond noir',
+				image : url+'/../../../custom/img/diapo_image_inv.png',
+				onclick : function() {
+                                    
+                                    var shortcode = "[diaporama titre='title' texte='Your text']<br />";
+                                        shortcode += "[slide]<br />";
+                                        shortcode += "insérer l'image<br />";
+                                        shortcode += "[/slide]<br />";
+                                        shortcode += "[/diaporama]";
+                                        
+                                        
+					ed.execCommand('mceInsertContent', false, shortcode);
+				}
+			});
+		},
+		createControl : function(n, cm) {
+			return null;
+		}
+	});
+	tinymce.PluginManager.add('diaposimplenoir', tinymce.plugins.Diaposimplenoir);
+})();
